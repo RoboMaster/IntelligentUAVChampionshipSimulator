444a2334d153309b5abfdd11abf124f58f52a8c3
jake.luo <jake.luo@dji.com>
2022-08-05 19:29:09 +0800

    master initial

--- stage1_RGBD/contest2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-UE4_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
-UE4_PROJECT_ROOT=$(dirname "$UE4_TRUE_SCRIPT_NAME")
-chmod +x "$UE4_PROJECT_ROOT/contest2/Binaries/Linux/contest2"
-"$UE4_PROJECT_ROOT/contest2/Binaries/Linux/contest2" contest2 "$@" 
--- stage1_RGBD/simulator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "launch simulator."
-gnome-terminal --tab --title="Simulator" -- /bin/bash -c "/bin/bash ./contest2.sh -WINDOWED -ResX=640 -ResY=480" \
-echo "launch roswrapper."
-gnome-terminal --tab --title="Roswrapper_Core"  -- /bin/bash -c "sleep 5;export ROS_IP=172.17.0.1;export ROS_MASTER_URI=http://localhost:11311;source /opt/ros/noetic/setup.sh;source $PWD/roswrapper/ros/devel/setup.bash;roslaunch airsim_ros_pkgs airsim_node.launch;exec bash"
